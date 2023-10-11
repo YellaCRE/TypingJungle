11daa2fb2fd944e81b91a09ef49e73baf6448413
Changjo Hwang <hchj2005@gmail.com>
2023-10-12 00:49:09 +0900

    1012 commit

--- init.py
+++ init.py
@@ -12,9 +12,25 @@ def mkdb():
     db.users.insert_one({'pid': '3', 'id': 'id3', 'pw': 'pw3', 'name': 'user3'})
 
     # rankingDB
+    db.ranking.insert_one({'pid': '1','score': '1'})
+    db.ranking.insert_one({'pid': '2','score': '2'})
+    db.ranking.insert_one({'pid': '3','score': '3'})
+    db.ranking.insert_one({'pid': '1','score': '4'})
+    db.ranking.insert_one({'pid': '2','score': '5'})
+    db.ranking.insert_one({'pid': '3','score': '6'})
+    db.ranking.insert_one({'pid': '1','score': '7'})
+    db.ranking.insert_one({'pid': '2','score': '8'})
+    db.ranking.insert_one({'pid': '3','score': '9'})
+    db.ranking.insert_one({'pid': '1','score': '10'})
+    db.ranking.insert_one({'pid': '2','score': '11'})
+    db.ranking.insert_one({'pid': '3','score': '12'})
+    db.ranking.insert_one({'pid': '1','score': '13'})
+    db.ranking.insert_one({'pid': '2','score': '14'})
+    db.ranking.insert_one({'pid': '3','score': '10000'})
     db.ranking.insert_one({'pid': '1','score': '100'})
     db.ranking.insert_one({'pid': '2','score': '1000'})
     db.ranking.insert_one({'pid': '3','score': '10000'})
+
     
     # logDB
     db.log.insert_one({'pid': '1','score': '100', 'time': '200'})

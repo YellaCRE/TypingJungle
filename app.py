89f53fe83e52061ca0d5b7a8a84049bbf29e76b9
Changjo Hwang <hchj2005@gmail.com>
2023-10-12 16:49:30 +0900

    commit 10121649

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
-from flask import Flask, request, render_template, jsonify, redirect, url_for
+from flask import Flask, request, render_template, jsonify
+import jwt, datetime
 
 app = Flask(__name__)
 
@@ -16,7 +17,6 @@ def start():
 @app.route('/login')
 def login():
     return render_template('login.html')
-
 
 @app.route('/signup')
 def signup():
@@ -60,10 +60,37 @@ def api_login():
     
     result = db.users.find_one({'id': id_receive, 'pw': pw_receive})
 
-    if result is not None:
-        return jsonify({'result': 'success'})
+    if result:
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
+        }
+        # token을 클라이언트에게 전달.
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+# [유저 정보 확인 API]
+# 로그인된 유저만 call 할 수 있음.
+@app.route('/api/token', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        # token을 시크릿키로 디코딩
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+        print(token_receive)
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+        # 여기에선 그 예로 닉네임을 보내주겠습니다.
+        userinfo = db.users.find_one({'id': payload['id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'id': userinfo['id']})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 # [랭킹 API]
 @app.route('/api/rank', methods=['GET'])
@@ -87,7 +114,6 @@ def api_rank():
         top10_ls = ranking_ls[:10]
     else:
         top10_ls = ranking_ls
-    
     return jsonify({'rank': top10_ls})
 
 # [결과 API]
